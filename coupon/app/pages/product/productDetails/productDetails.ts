1499c572428a7d2bb14332f54925633aa6c0dc62
劉超 <emacs157@yahoo.com>
2016-06-26 02:03:30 +0900

    popOverList

--- coupon/app/pages/product/productDetails/productDetails.ts
+++ coupon/app/pages/product/productDetails/productDetails.ts
@@ -1,21 +1,58 @@
 /**
  * Created by liuchao on 6/25/16.
  */
-import {Component} from '@angular/core';
-import {NavController, NavParams} from 'ionic-angular';
+import {Component, ViewChild, ElementRef} from '@angular/core';
+import {App, NavController, NavParams, Popover,} from 'ionic-angular';
+import {Category} from "./popoverPages/category";
+import {Location} from "./popoverPages/location";
+import {Order} from "./popoverPages/order";
 
 @Component({
     templateUrl: 'build/pages/product/productDetails/productDetails.html',
 })
 export class ProductDetails {
+    @ViewChild('popoverContent', {read: ElementRef}) content: ElementRef;
+    @ViewChild('popoverText', {read: ElementRef}) text: ElementRef;
     product;
-    pet;
 
-
-
-    constructor(params: NavParams) {
+    constructor(private params: NavParams,private nav:NavController) {
         this.product = params.data.product;
-        this.pet = "kittens";
         console.log(params.data);
+    }
+
+    presentCategoryPopover(ev) {
+        let category = Popover.create(Category, {
+            contentEle: this.content.nativeElement,
+            textEle: this.text.nativeElement
+        });
+
+        console.log("presentPopover");
+        this.nav.present(category, {
+            ev: ev
+        });
+    }
+
+    presentLocationPopover(ev) {
+        let location = Popover.create(Location, {
+            contentEle: this.content.nativeElement,
+            textEle: this.text.nativeElement
+        });
+
+        console.log("presentPopover");
+        this.nav.present(location, {
+            ev: ev
+        });
+    }
+
+    presentOrderPopover(ev) {
+        let order = Popover.create(Order, {
+            contentEle: this.content.nativeElement,
+            textEle: this.text.nativeElement
+        });
+
+        console.log("presentPopover");
+        this.nav.present(order, {
+            ev: ev
+        });
     }
 }
